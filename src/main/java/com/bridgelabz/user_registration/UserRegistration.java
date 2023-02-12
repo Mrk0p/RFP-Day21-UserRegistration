f97763f5f5c5d882e8951bcbd23b3b045f0f4c10
Mrk0p <sachinwele23@gmail.com>
2023-02-12 20:31:38 +0530

    Added : Test junit parametrised detail

--- src/main/java/com/bridgelabz/user_registration/UserRegistration.java
+++ src/main/java/com/bridgelabz/user_registration/UserRegistration.java
@@ -4,32 +4,8 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
-    private static final String EMAIL = "^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$";
-    private static final String PHONE_NUMBER = "^[\\d]{2}[\\s][\\d]{10}$";
-    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=[^!@#$%^&+=]*[!@#$%^&+=][^!@#$%^&+=]*$)(?=.*[0-9]).{8,}$";
-
-    public boolean firstNameValidate(String firstName)
+    public boolean validateEmail(String email)
     {
-        return Pattern.matches(NAME, firstName);
-    }
-    public boolean lastNameValidate(String lastName)
-    {
-        return Pattern.matches(NAME, lastName);
-    }
-
-    public boolean emailValidate(String email)
-    {
-        return Pattern.matches(EMAIL, email);
-    }
-
-    public boolean phoneNumberValidate(String phoneNum)
-    {
-        return Pattern.matches(PHONE_NUMBER, phoneNum);
-    }
-
-    public boolean passwordValidate(String password)
-    {
-        return Pattern.matches(PASSWORD, password);
+        return Pattern.matches("^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", email);
     }
 }
--- src/test/java/com/bridgelabz/user_registration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/user_registration/UserRegistrationTest.java
@@ -1,77 +1,43 @@
 package com.bridgelabz.user_registration;
 
+import org.junit.Before;
 import org.junit.Test;
-import junit.framework.Assert;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserRegistrationTest {
 
-    @Test
-    public void givenFirstName_ShouldReturnTrue() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.firstNameValidate("Rajkumar");
-        Assert.assertTrue(result);
+    private String emailTest;
+    private boolean expectedResult;
+    private UserRegistration validateEmail;
+
+    public UserRegistrationTest(String email, boolean expectedResult) {
+        this.emailTest = email;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void initialize() {
+        validateEmail = new UserRegistration();
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
+                { "abc.100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
+                { "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
+                { "abc+100@gmail.com", true }, { "abc", false }, { "abc@.com.my", false }, { "abc123@gmail.a", false },
+                { "abc123@.com", false }, { "abc123@.com.com", false }, { ".abc@abc.com", false },
+                { "abc()*@gmail.com", false }, { "abc@%*.com", false }, { "abc..2002@gmail.com", false },
+                { "abc.@gmail.com", false }, { "abc@abc@gmail.com", false }, { "abc@gmail.com.1a", false },
+                { "abc@gmail.com.aa.au", false } });
     }
 
     @Test
-    public void givenFirstName_ShouldReturnFalse() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.firstNameValidate("rajkumar");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenLastName_ShouldReturnTrue() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.lastNameValidate("Verma");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenLastName_ShouldReturnFalse() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.lastNameValidate("verma");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenEmail_ShouldReturnTrue() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.co.in");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenEmail_ShouldReturnFalse() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.in");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenPhone_ShouldReturnTrue() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.phoneNumberValidate("91 8954653285");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenPhone_ShouldReturnFalse() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.phoneNumberValidate("8954653285");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenPassword_ShouldReturnTrue() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.passwordValidate("Abc12345#");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenPassword_ShouldReturnFalse() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.passwordValidate("Rajubahi23#%");
-        Assert.assertFalse(result);
+    public void givenEmailAsVar_ShouldReturnTrueorFalse() {
+        assertEquals(expectedResult, validateEmail.validateEmail(emailTest));
     }
 }
